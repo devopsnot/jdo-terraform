632553a8be57ce3dff2f2c500a62c9aa67e150fd
devopsnot <eduardobsn.exe@gmail.com>
2023-04-17 00:53:38 +0000

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
@@ -9,22 +9,22 @@ terraform {
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = "dop_v1_d10c07580ae0a41f4ec003e375e2a0bb6ecd32bafa329dee091c5f87e3a3e0da"
+  token = var.do_token
 }
 
 # MAQUINA VIRTUAL CREATE
 # Create a new Web Droplet in the nyc2 region
 resource "digitalocean_droplet" "jenkins" {
-  image  = "ubuntu-22-04-x64"
-  name   = "jenkins-vm"
-  region = "nyc1"
-  size   = "s-2vcpu-2gb"
-  ssh_keys = [data.digitalocean_ssh_key.example.id]
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins"
+  region   = var.region
+  size     = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
 }
 
 # CONECTION SSH
-data "digitalocean_ssh_key" "example" {
-  name = "srv-ubuntu-vm"
+data "digitalocean_ssh_key" "ssh_key" {
+  name = var.ssh_key_name
 }
 
 
@@ -34,7 +34,7 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
   name   = "k8s"
   region = "nyc1"
   # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.24.4-do.0"
+  version = "1.26.3-do.0"
 
   node_pool {
     name       = "default"
@@ -42,4 +42,16 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
     node_count = 2
 
   }
+}
+
+variable "do_token" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
+
+variable "ssh_key_name" {
+  default = ""
 }
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+do_token     = "dop_v1_dfb6f9fbcc63d38a299a235aa54712f5dd43e6ac34a9e0770418591f4133ae91"
+region       = "nyc1"
+ssh_key_name = "srv-ubuntu-vm" 
